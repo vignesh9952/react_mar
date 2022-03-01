b27c93b7345a9d8758c8d7aef7d646c464593cc1
VIGNESH R <93066467+vignesh9952@users.noreply.github.com>
2022-03-01 20:30:49 +0530

    Add files via upload

---
+++ laptop.js
@@ -0,0 +1,20 @@
+ function Laptop(Props){
+    return(
+        <div>
+			<img
+				src={Props.imagelink}
+				alt="laptop"
+				width="350"
+				height="350"
+			/>
+			<h3>{Props.imagename}</h3>
+			<h3>Free COD</h3>
+			<h4>RS:<small><del>80,0000</del></small></h4>
+			<h4>DIS price:{Props.price}</h4>
+</div>
+    );
+}
+
+
+
+export default Laptop;
